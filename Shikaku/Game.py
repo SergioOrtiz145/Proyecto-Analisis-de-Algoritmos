ada616b3102f04c185cb0f0e22ce9140379ebb49
SergioOrtiz145 <selgamerv@gmail.com>
2026-05-16 10:58:43 -0500

    feat: se agrego la clase Game para realizar acciones con el tablero

---
+++ Shikaku/Game.py
@@ -0,0 +1,46 @@
+import copy
+from .Board import Board
+
+class Game:
+
+    m_Board  = None
+    m_Player = None
+
+    def __init__( self, w, h, hints, player ):
+        self.m_Board  = Board( w, h, hints )
+        self.m_Player = player
+        self.m_Player.init( w, h, hints )
+
+    def solve( self ):
+        while not self.m_Board.finished():
+            print( str( self.m_Board ) )
+            print( '─' * 42 )
+
+            # el jugador decide qué rectángulo colocar
+            accion = self.m_Player.play()
+
+            # procesar la acción
+            if accion is None:
+                # jugador abandona
+                break
+
+            tipo = accion.get( 'tipo' )
+
+            if tipo == 'colocar':
+                r1, c1, r2, c2 = accion[ 'coords' ]
+                self.m_Board.place( r1, c1, r2, c2 )
+
+            elif tipo == 'eliminar':
+                id_reg = accion[ 'id' ]
+                self.m_Board.remove( id_reg )
+
+            elif tipo == 'noop':
+                pass
+            
+        print( str( self.m_Board ) )
+        print( '─' * 42 )
+
+        if self.m_Board.has_won():
+            print( '¡Ganaste! :-D  El tablero está completo.' )
+        else:
+            print( 'El tablero está incompleto' )
